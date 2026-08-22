dbc413fd3b90e97a42ad7181ee9f028f23588ee9
Reeve M. Philip <reevemphilip@gmail.com>
2026-08-22 16:31:12 -0400

    Set up Playwright Typescript test suite

---
+++ tests/todo.spec.ts
@@ -0,0 +1,14 @@
+import {test, expect} from '@playwright/test';
+
+test('user an add a todo item', async ({ page }) => {
+    await page.goto('https://demo.playwright.dev/todomvc/');
+
+    const newTodo = page.getByPlaceholder("What needs to be done?");
+
+    await newTodo.fill('Learn Playwright');
+
+    await newTodo.press('Enter');
+
+    await expect(page.getByTestId('todo-item')).toContainText('Learn Playwright')
+
+});
