48b29101383b1b1a3bd6b0ae2f38bded73b66d5f
Reeve M. Philip <reevemphilip@gmail.com>
2026-08-22 16:35:34 -0400

    Add completed todo test

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -12,3 +12,18 @@ test('user an add a todo item', async ({ page }) => {
     await expect(page.getByTestId('todo-item')).toContainText('Learn Playwright')
 
 });
+
+test('user can complete a todo item', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc/');
+
+  const newTodo = page.getByPlaceholder('What needs to be done?');
+
+  await newTodo.fill('Learn Playwright');
+  await newTodo.press('Enter');
+
+  const todoItem = page.getByTestId('todo-item');
+
+  await todoItem.getByRole('checkbox').check();
+
+  await expect(todoItem).toHaveClass(/completed/);
+});
